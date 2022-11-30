31295b2c7bf96f4247395b2833a1569f67115985
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-11-30 17:22:06 -0500

    added middleware, route to home page and listening port.

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+// requires express and path core module
 const express = require('express');
 const path = require('path');
 
+// allows us to use express object and establishes port const
+const app = express();
 const PORT = process.env.port || 3001;
 
-const app = express();
+// middleware that states where to serve static data from
+app.use(express.static('public'));
+
+// route to homepage n
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+// server :)
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
