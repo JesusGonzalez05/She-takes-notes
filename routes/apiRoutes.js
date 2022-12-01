f296c5f0b42d404367fbe53ffc238333b24a2a3b
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-11-30 19:48:55 -0500

    i struggled with seperating api routes so I kept in server.js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,11 @@
 // import express package as router
-const apiRoute = require('express').Router();
+const apiRoutes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fs');
 
 
 
 
 // Route for retrieving all saved notes
-apiRoute.get('/api/notes', (req, res) =>
+apiRoutes.get('/', (req, res) =>
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
+
 
 // allows us to use express object and establishes port const
 const app = express();
@@ -9,6 +11,8 @@ const PORT = process.env.port || 3001;
 
 // middleware that states where to serve static data from
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // route to homepage n
 app.get('/', (req, res) =>
@@ -17,6 +21,21 @@ app.get('/', (req, res) =>
 // route to notes page (where you enter your notes)
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+);
+
+
+
+// Route for retrieving all saved notes
+app.get('/api/notes', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
 // server :)
