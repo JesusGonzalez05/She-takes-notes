5afc28ca93844cb8428582559690dc9d810b1e6a
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-11-30 19:37:38 -0500

    required helpers in api routes file.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,11 @@
 // import express package as router
 const apiRoute = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fs');
+
 
 
 
 // Route for retrieving all saved notes
-router.get('/api/notes', (req, res) =>
+apiRoute.get('/api/notes', (req, res) =>
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
